26f49c184c7a337bef6ea8c191db889338bafb54
Misc1170 <sloopy1170@gmail.com>
2023-12-23 19:01:13 +0500

    lab1 done

---
+++ lab1-2/lab1-2.cs
@@ -0,0 +1,22 @@
+﻿int x = -7, y = 0;
+
+Console.Write("Введите число X: ");
+string numberForX = Console.ReadLine();
+int checkX = Convert.ToInt32(numberForX);
+
+Console.Write("Введите число Y: ");
+string numberForY = Console.ReadLine();
+int checkY = Convert.ToInt32(numberForY);
+
+bool isInRange = false;
+
+if (
+    checkX > x 
+    && checkX < 0 
+    && checkY == y
+    )
+{
+    isInRange = true;
+}
+
+Console.WriteLine(isInRange);
---
+++ lab1/lab1-1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int n = 0;
+int m = 0;
+
+// №1
+Console.WriteLine(++n * ++m);
+
+// №2
+Console.WriteLine(m++ < n);
+
+// №3
+Console.WriteLine(n++ > m);
+
+
+// №4
+Console.Write("Введите число X: ");
+string numForX = Console.ReadLine();
+int x = Convert.ToInt32(numForX);
+double resultX = 0;
+resultX = x + (1 / (Math.Pow(x, 2) - x)) - 2;
+Console.WriteLine(resultX);
